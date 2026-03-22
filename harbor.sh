1f9405f1c29f21776f5b6e49bd48c87bd68d4c2a
dreamsvm-service <svmservice@dreamcube.pl>
2026-03-22 20:26:34 +0100

    Refactor harbor.sh for Ubuntu and Alpine setup

--- harbor.sh
+++ harbor.sh
@@ -1,109 +1,115 @@
 #!/bin/sh
 
 #############################
-# Alpine Linux Installation #
+# CONFIG
 #############################
 
-# Define the root directory to /home/container.
-# We can only write in /home/container and /tmp in the container.
 ROOTFS_DIR=/home/container
 
-# Define the Alpine Linux version we are going to be using.
-ALPINE_VERSION="3.9"
-ALPINE_FULL_VERSION="3.9.6"
-APK_TOOLS_VERSION="2.14.0-r2" # Make sure to update this too when updating Alpine Linux.
-PROOT_VERSION="5.3.0" # Some releases do not have static builds attached.
+# Wybierz distro: alpine lub ubuntu
+DISTRO="alpine"
 
-# Detect the machine architecture.
+#############################
+# ARCH DETECTION
+#############################
+
 ARCH=$(uname -m)
 
-# Check machine architecture to make sure it is supported.
-# If not, we exit with a non-zero status code.
 if [ "$ARCH" = "x86_64" ]; then
   ARCH_ALT=amd64
 elif [ "$ARCH" = "aarch64" ]; then
   ARCH_ALT=arm64
 else
-  printf "Unsupported CPU architecture: ${ARCH}"
+  echo "Unsupported architecture: $ARCH"
   exit 1
 fi
 
-# Download & decompress the Alpine linux root file system if not already installed.
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-    # Download Alpine Linux root file system.
-    curl -Lo /tmp/rootfs.tar.xz \
-    "https://github.com/termux/proot-distro/releases/download/v4.7.0/ubuntu-jammy-x86_64-pd-v4.7.0.tar.xz"
-    
-    # Extract the Alpine Linux root file system.
-    tar -xvf /tmp/rootfs.tar.xz -C $ROOTFS_DIR
-fi
+#############################
+# UBUNTU SETUP (x86_64 only)
+#############################
 
-################################
-# Package Installation & Setup #
-################################
+if [ "$DISTRO" = "ubuntu" ]; then
+  if [ "$ARCH" != "x86_64" ]; then
+    echo "Ubuntu supported only on x86_64"
+    exit 1
+  fi
 
-# Download static APK-Tools temporarily because minirootfs does not come with APK pre-installed.
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-    # Download the packages from their sources.
-    curl -Lo /tmp/apk-tools-static.apk "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/${ARCH}/apk-tools-static-${APK_TOOLS_VERSION}.apk"
-    curl -Lo /tmp/gotty.tar.gz "https://github.com/sorenisanerd/gotty/releases/download/v1.5.0/gotty_v1.5.0_linux_${ARCH_ALT}.tar.gz"
-    curl -Lo $ROOTFS_DIR/usr/local/bin/proot "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
-    # Extract everything that needs to be extracted.
-    tar -xzf /tmp/apk-tools-static.apk -C /tmp/
-    tar -xzf /tmp/gotty.tar.gz -C $ROOTFS_DIR/usr/local/bin
-    # Install base system packages using the static APK-Tools.
-    /tmp/sbin/apk.static -X "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/" -U --allow-untrusted --root $ROOTFS_DIR add alpine-base apk-tools
-    # Make PRoot and GoTTY executable.
-    chmod 755 $ROOTFS_DIR/usr/local/bin/proot $ROOTFS_DIR/usr/local/bin/gotty
-fi
+  if [ ! -e $ROOTFS_DIR/.installed ]; then
+    echo "Downloading Ubuntu rootfs..."
 
-# Clean-up after installation complete & finish up.
-if [ ! -e $ROOTFS_DIR/.installed ]; then
-    # Add DNS Resolver nameservers to resolv.conf.
-    printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > ${ROOTFS_DIR}/etc/resolv.conf
-    # Wipe the files we downloaded into /tmp previously.
-    rm -rf /tmp/apk-tools-static.apk /tmp/rootfs.tar.gz /tmp/sbin
-    # Create .installed to later check whether Alpine is installed.
+    curl -Lo /tmp/ubuntu.tar.xz \
+      "https://github.com/termux/proot-distro/releases/download/v4.7.0/ubuntu-jammy-x86_64-pd-v4.7.0.tar.xz"
+
+    mkdir -p $ROOTFS_DIR
+    tar -xJf /tmp/ubuntu.tar.xz -C $ROOTFS_DIR
+
     touch $ROOTFS_DIR/.installed
+  fi
+
+  exec proot \
+    --rootfs="$ROOTFS_DIR" \
+    --link2symlink \
+    --kill-on-exit \
+    --root-id \
+    --cwd=/root \
+    --bind=/proc \
+    --bind=/dev \
+    --bind=/sys \
+    --bind=/tmp \
+    /bin/bash
+
+  exit 0
 fi
 
-# Print some useful information to the terminal before entering PRoot.
-# This is to introduce the user with the various Alpine Linux commands.
-clear && cat << EOF
+#############################
+# ALPINE SETUP (default)
+#############################
 
- ██╗  ██╗ █████╗ ██████╗ ██████╗  ██████╗ ██████╗ 
- ██║  ██║██╔══██╗██╔══██╗██╔══██╗██╔═══██╗██╔══██╗
- ███████║███████║██████╔╝██████╔╝██║   ██║██████╔╝
- ██╔══██║██╔══██║██╔══██╗██╔══██╗██║   ██║██╔══██╗
- ██║  ██║██║  ██║██║  ██║██████╔╝╚██████╔╝██║  ██║
- ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝  ╚═════╝ ╚═╝  ╚═╝
- 
- Welcome to Alpine Linux minirootfs!
- This is a lightweight and security-oriented Linux distribution that is perfect for running high-performance applications.
- 
- Here are some useful commands to get you started:
- 
-    apk add [package] : install a package
-    apk del [package] : remove a package
-    apk update : update the package index
-    apk upgrade : upgrade installed packages
-    apk search [keyword] : search for a package
-    apk info [package] : show information about a package
-    gotty -p [server-port] -w ash : share your terminal
- 
- If you run into any issues make sure to report them on GitHub!
- https://github.com/RealTriassic/Harbor
- 
-EOF
+ALPINE_VERSION="3.9"
+ALPINE_FULL_VERSION="3.9.6"
+APK_TOOLS_VERSION="2.14.0-r2"
+PROOT_VERSION="5.3.0"
 
-###########################
-# Start PRoot environment #
-###########################
+if [ "$ARCH" = "x86_64" ]; then
+  ALPINE_ARCH=amd64
+elif [ "$ARCH" = "aarch64" ]; then
+  ALPINE_ARCH=arm64
+fi
 
-# This command starts PRoot and binds several important directories
-# from the host file system to our special root file system.
-$ROOTFS_DIR/usr/local/bin/proot \
---rootfs="${ROOTFS_DIR}" \
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  curl -Lo /tmp/rootfs.tar.gz \
+  "https://dl-cdn.alpinelinux.org/v${ALPINE_VERSION}/releases/${ARCH}/alpine-minirootfs-${ALPINE_FULL_VERSION}-${ARCH}.tar.gz"
+
+  tar -xzf /tmp/rootfs.tar.gz -C $ROOTFS_DIR
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  curl -Lo /tmp/apk-tools-static.apk \
+  "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/${ARCH}/apk-tools-static-${APK_TOOLS_VERSION}.apk"
+
+  curl -Lo $ROOTFS_DIR/usr/local/bin/proot \
+  "https://github.com/proot-me/proot/releases/download/v${PROOT_VERSION}/proot-v${PROOT_VERSION}-${ARCH}-static"
+
+  tar -xzf /tmp/apk-tools-static.apk -C /tmp/
+
+  /tmp/sbin/apk.static \
+    -X "https://dl-cdn.alpinelinux.org/alpine/v${ALPINE_VERSION}/main/" \
+    -U --allow-untrusted \
+    --root $ROOTFS_DIR add alpine-base apk-tools
+
+  chmod 755 $ROOTFS_DIR/usr/local/bin/proot
+fi
+
+if [ ! -e $ROOTFS_DIR/.installed ]; then
+  printf "nameserver 1.1.1.1\nnameserver 1.0.0.1" > $ROOTFS_DIR/etc/resolv.conf
+  rm -rf /tmp/apk-tools-static.apk /tmp/rootfs.tar.gz /tmp/sbin
+  touch $ROOTFS_DIR/.installed
+fi
+
+echo "Starting Alpine..."
+
+exec $ROOTFS_DIR/usr/local/bin/proot \
+--rootfs="$ROOTFS_DIR" \
 --link2symlink \
 --kill-on-exit \
 --root-id \
